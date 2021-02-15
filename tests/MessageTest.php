b4c3a407ecaf7f0b22045f75bfa70f223acfc2c3
Nepster <nepsterxxx@mail.ru>
2021-02-15 16:26:50 +0200

    add demo tests

---
+++ tests/MessageTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Simple\QueueTest;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class MessageTest
+ * @package Simple\QueueTest
+ */
+class MessageTest extends TestCase
+{
+
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
+
+}
