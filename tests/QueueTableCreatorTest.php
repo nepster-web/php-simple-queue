52e7dbbf4c2a54e2f30df5555b86ba1b9613bbd8
Nepster <nepsterxxx@mail.ru>
2021-02-19 12:24:45 +0200

    fix: add test QueueTableCreatorTest

--- tests/QueueTableCreatorTest.php
+++ tests/QueueTableCreatorTest.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Simple\QueueTest;
 
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\Table;
 use PHPUnit\Framework\TestCase;
+use Simple\Queue\QueueTableCreator;
+use Simple\QueueTest\Helper\FakeConnectionTrait;
 
 /**
  * Class QueueTableCreatorTest
@@ -12,5 +16,48 @@ use PHPUnit\Framework\TestCase;
  */
 class QueueTableCreatorTest extends TestCase
 {
+    use FakeConnectionTrait;
+
+    public function testChangeTableName(): void
+    {
+        QueueTableCreator::changeTableName('my_table');
+
+        self::assertEquals('my_table', QueueTableCreator::getTableName());
+    }
+
+    public function testSimulateTableCreation(): void
+    {
+        $data = [];
+
+        $queueTableCreator = new QueueTableCreator($this->getFakeConnection($data));
+
+        $queueTableCreator->createDataBaseTable();
+
+        /** @var Table $table */
+        $table = $data['createTable'];
+
+        $tableColumns = [];
+
+        /** @var Column $column */
+        foreach ($table->getColumns() as $name => $column) {
+            $tableColumns[$name] = $column->getType()->getName();
+        }
+
+        $expected = [
+            'id' => 'guid',
+            'status' => 'string',
+            'attempts' => 'smallint',
+            'queue' => 'string',
+            'event' => 'string',
+            'body' => 'text',
+            'priority' => 'smallint',
+            'error' => 'text',
+            'redelivered_at' => 'datetime_immutable',
+            'created_at' => 'datetime_immutable',
+            'exact_time' => 'bigint',
+        ];
+
+        self::assertEquals($expected, $tableColumns);
+    }
 
 }
