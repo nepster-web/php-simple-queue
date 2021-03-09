d2b5091269f0876dda08696f5e87f45b90b62b23
DenysSolomakha <denis.solomakha94@gmail.com>
2021-03-09 11:54:44 +0200

    improve example produce

--- example/produce.php
+++ example/produce.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 include __DIR__ . '/../vendor/autoload.php';
 
-
 $connection = \Doctrine\DBAL\DriverManager::getConnection([
     'driver' => 'pdo_sqlite',
     'path' => '/db/queue.db'
 ]);
 
-$producer = new \Simple\Queue\Producer($connection);
+$producer = new \Simple\Queue\Producer($connection, null);
 
 
 echo 'Start send to queue' . PHP_EOL;
