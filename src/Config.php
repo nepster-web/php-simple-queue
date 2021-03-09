8b06d9c1e341206ab4727620f8a1a0b94ed68243
DenysSolomakha <denis.solomakha94@gmail.com>
2021-03-09 11:48:21 +0200

    improve class Config

--- src/Config.php
+++ src/Config.php
@@ -6,6 +6,8 @@ namespace Simple\Queue;
 
 use RuntimeException;
 use InvalidArgumentException;
+use Simple\Queue\Serializer\SymfonySerializer;
+use Simple\Queue\Serializer\SerializerInterface;
 
 /**
  * Class Config
@@ -14,13 +16,80 @@ use InvalidArgumentException;
 class Config
 {
     /** @var int in seconds */
-    public int $redeliveryTimeInSeconds = 180;
+    private int $redeliveryTimeInSeconds = 180;
 
     /** @var int */
-    public int $numberOfAttemptsBeforeFailure = 5;
+    private int $numberOfAttemptsBeforeFailure = 5;
 
     /** @var array */
-    public array $jobs = [];
+    private array $jobs = [];
+
+    /** @var SerializerInterface|null */
+    private ?SerializerInterface $serializer = null;
+
+    /**
+     * Config constructor.
+     */
+    public function __construct()
+    {
+        if ($this->serializer === null) {
+            $this->serializer = new SymfonySerializer();
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getRedeliveryTimeInSeconds(): int
+    {
+        return $this->redeliveryTimeInSeconds;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumberOfAttemptsBeforeFailure(): int
+    {
+        return $this->numberOfAttemptsBeforeFailure;
+    }
+
+    /**
+     * @return array
+     */
+    public function getJobs(): array
+    {
+        return $this->jobs;
+    }
+
+    /**
+     * @param string $key
+     * @return string|null
+     */
+    public function getJob(string $key): ?string
+    {
+        if ($this->hasJob($key) === false) {
+            throw new InvalidArgumentException(sprintf('Job "%s" doesn\'t exists.', $key));
+        }
+
+        return $this->jobs[$key];
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function hasJob(string $key): bool
+    {
+        return isset($this->jobs[$key]);
+    }
+
+    /**
+     * @return static
+     */
+    public static function getDefault(): self
+    {
+        return new self;
+    }
 
     /**
      * @param int $seconds
@@ -62,5 +131,22 @@ class Config
         $this->jobs[$alias] = $class;
 
         return $this;
+    }
+
+    /**
+     * @return SerializerInterface
+     */
+    public function getSerializer(): SerializerInterface
+    {
+        return $this->serializer;
+    }
+
+    /**
+     * @param SerializerInterface $serializer
+     * @return $this
+     */
+    public function withSerializer(SerializerInterface $serializer): self
+    {
+        $this->serializer = $serializer;
     }
 }
