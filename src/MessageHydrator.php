b8848203ee7d40cf6f006bfbe436db9bc4e4f04e
DenysSolomakha <denis.solomakha94@gmail.com>
2021-03-09 11:53:39 +0200

    message creation moved to MessageHydrator

--- src/MessageHydrator.php
+++ src/MessageHydrator.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Simple\Queue;
 
+use Exception;
+use DateTimeImmutable;
 use Laminas\Hydrator\ReflectionHydrator;
+use Laminas\Hydrator\Strategy\HydratorStrategy;
 
 /**
  * Class MessageHydrator
@@ -36,12 +39,42 @@ class MessageHydrator
     }
 
     /**
+     * @return $this
+     */
+    public function jobable(): self
+    {
+        $this->message = $this->hydrate(['isJob' => true]);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function unJobable(): self
+    {
+        $this->message = $this->hydrate(['isJob' => false]);
+
+        return $this;
+    }
+
+    /**
      * @param string $error
      * @return $this
      */
-    public function changeError(string $error): self
+    public function setError(string $error): self
     {
         $this->message = $this->hydrate(['error' => $error]);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function increaseAttempt(): self
+    {
+        $this->hydrate(['attempts' => $this->message->getAttempts() + 1]);
 
         return $this;
     }
@@ -60,11 +93,38 @@ class MessageHydrator
      */
     private function hydrate(array $data): Message
     {
-        $hydrator = new ReflectionHydrator();
-
         /** @var Message $redeliveredMessage */
-        $redeliveredMessage = $hydrator->hydrate($data, $this->message);
+        $redeliveredMessage = (new ReflectionHydrator())->hydrate($data, $this->message);
 
         return $redeliveredMessage;
+    }
+
+    /**
+     * Create entity Message from array data
+     *
+     * @param array $data
+     * @return Message
+     * @throws Exception
+     */
+    public static function createMessage(array $data): Message
+    {
+        $strategy = new HydratorStrategy(new ReflectionHydrator(), Message::class);
+
+        /** @var Message $message */
+        $message = $strategy->hydrate(array_merge($data, [
+            "queue" => $data['queue'] ?? 'default',
+            "event" => $data['event'] ?? null,
+            "isJob" => $data['is_job'] ?? false,
+            "body" => $data['body'] ?? '',
+            "error" => $data['error'] ?? null,
+            "attempts" => $data['attempts'] ?? 0,
+            "status" => new Status($data['status'] ?? Status::NEW),
+            "priority" => new Priority((int)($data['priority'] ?? Priority::DEFAULT)),
+            "exactTime" => $data['exact_time'] ?? time(),
+            "createdAt" => new DateTimeImmutable($data['created_at'] ?? 'now'),
+            "redeliveredAt" => isset($data['redelivered_at']) ? new DateTimeImmutable($data['redelivered_at']) : null,
+        ]));
+
+        return $message;
     }
 }
