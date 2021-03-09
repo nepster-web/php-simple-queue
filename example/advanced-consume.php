eb3485a2bf5a7a158ecb7ac04d7cd25104ff7850
DenysSolomakha <denis.solomakha94@gmail.com>
2021-03-09 11:47:03 +0200

    improve advanced-consume

--- example/advanced-consume.php
+++ example/advanced-consume.php
@@ -7,11 +7,11 @@ include __DIR__ . '/../vendor/autoload.php';
 
 $connection = \Doctrine\DBAL\DriverManager::getConnection([
     'driver' => 'pdo_sqlite',
-    'path' => '/db/queue.db'
+    'path' => '/db/queue.db',
 ]);
 
-$producer = new \Simple\Queue\Producer($connection);
-$consumer = new \Simple\Queue\Consumer($connection, $producer);
+$producer = new \Simple\Queue\Producer($connection, null);
+$consumer = new \Simple\Queue\Consumer($connection, $producer, null);
 
 
 echo 'Start consuming' . PHP_EOL;
@@ -21,16 +21,9 @@ echo 'Start consuming' . PHP_EOL;
 $consumer->bind('my_queue', static function(\Simple\Queue\Message $message, \Simple\Queue\Producer $producer): string {
 
     // Your message handling logic
+    var_dump($message->getBody() . PHP_EOL);
 
-    return 'status';
-});
-
-// process all messages from queue with event
-$consumer->bind('my_queue.my_event', static function(\Simple\Queue\Message $message, \Simple\Queue\Producer $producer): string {
-
-    // Your message handling logic
-
-    return 'status';
+    return \Simple\Queue\Consumer::ACK;
 });
 
 $consumer->consume();
